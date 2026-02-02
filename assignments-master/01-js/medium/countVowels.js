f974ad6a18aee084f78ddf94e8177a1b4357c6bc
PUSHKAR PANT <pushkarpant0@PUSHKARs-MacBook-Air.local>
2026-02-02 20:05:06 +0530

    Add assignments-master folder with all files

---
+++ assignments-master/01-js/medium/countVowels.js
@@ -0,0 +1,18 @@
+/*
+  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+  Once you've implemented the logic, test your code by running
+*/
+
+function countVowels(str) {
+    let n = str.length;
+    let count = 0;
+    for(let i=0; i<n; i++){
+      if(s[i]==='a' || s[i]==='e' || s[i]==='i' || s[i]==='o' || s[i]==='u'  || s[i]==='A' || s[i]==='E' || s[i]==='I' || s[i]==='O' || s[i]==='U') 
+        count++;
+    }
+    return count;
+}
+
+module.exports = countVowels;
